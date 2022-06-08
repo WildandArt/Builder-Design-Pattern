3bafc039074ee82e72883b19d7b86f9a291428c1
Art <WildandArt@users.noreply.github.com>
2022-06-08 10:54:40 +0300

    Initial Commit

---
+++ Builder Design Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Builder Design Pattern
+//
+//  Created by Artemy Ozerski on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
